c4fd8fd534623c9be142ab727574329fa57a08f6
Fkm_X3 <203760409+fkm-X3@users.noreply.github.com>
2026-07-17 15:12:47 +1000

    Settings page

--- Cube-Combat/Scene/Game/main_menu.gd
+++ Cube-Combat/Scene/Game/main_menu.gd
@@ -16,7 +16,7 @@ func _on_game_start_pressed() -> void:
 
 
 func _on_settings_pressed() -> void:
-	print("Settings pressed")
+	get_tree().change_scene_to_file("res://Scene/Menu's/settings.tscn")
 
 
 func _on_exit_pressed() -> void:
---
+++ Cube-Combat/Scene/Menu's/volume.gd
@@ -0,0 +1,23 @@
+extends HSlider
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, value)
+
+
+func _on_mute_toggle_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0, toggled_on)
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
