00620ee3607bc3c11b41a891774c3884d6a45a4d
Fkm_X3 <203760409+fkm-X3@users.noreply.github.com>
2026-07-18 19:14:47 +1000

    Chore: updated mainmenu to use cube select menu

--- Cube-Combat/game/main_menu.gd
+++ Cube-Combat/game/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_game_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://game/main.tscn")
+	get_tree().change_scene_to_file("res://menu's/cube select.tscn")
 
 
 func _on_settings_pressed() -> void:
